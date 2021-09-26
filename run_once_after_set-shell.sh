99355cee87ddf8d99432410f9a82113b56e6a983
Ross Light <ross@zombiezen.com>
2021-09-26 12:15:14 -0700

    Add scripts to set default shell and install Debian packages

---
+++ run_once_after_set-shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -euo pipefail
+want=/bin/zsh
+if [[ -x "$want" && "$( basename "$SHELL" )" != "$( basename "$want" )" ]]; then
+  echo "** Setting shell..." 1>&2
+  chsh -s "$want"
+fi
