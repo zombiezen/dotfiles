22d94da8d4b609ee8df6487a87202665b5d18f07
Ross Light <ross@zombiezen.com>
2022-01-24 19:57:00 -0800

    nix: install glibcLocales for en_US and C.UTF-8
    Move package set into Nix expressions instead of bash

--- nix/default.nix
+++ nix/default.nix
@@ -1,4 +1,4 @@
-{
+rec {
   nixpkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/6f05cfdb1e78d36c0337516df674560e4b51c79b.tar.gz") {
     overlays = [
       (self: super: {
@@ -6,5 +6,48 @@
         redo-zombiezen = super.callPackage ./redo-zombiezen.nix {};
       })
     ];
+  };
+
+  mypkgs = with nixpkgs; {
+    inherit
+      _1password
+      age
+      cargo
+      colordiff
+      crane
+      delve
+      gcrane
+      gg-scm
+      git
+      gnupg
+      go-font
+      go_1_17
+      gohack
+      google-cloud-sdk
+      gopls
+      graphviz-nox
+      heroku
+      hugo
+      jq
+      ledger
+      mercurial
+      netcat-gnu
+      nodejs-16_x
+      redo-zombiezen
+      rust-analyzer
+      rustc
+      rustfmt
+      shellcheck
+      subversionClient
+      sqlite-interactive
+      terraform
+      tree
+      vim_configurable
+    ;
+  } // lib.optionalAttrs (!builtins.isNull glibcLocales) {
+    glibcLocales = glibcLocales.override {
+      allLocales = false;
+      locales = [ "C.UTF-8/UTF-8" "en_US.UTF-8/UTF-8" ];
+    };
   };
 }
