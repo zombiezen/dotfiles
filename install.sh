74b3aa1caff50439068c32242a3ead536e67d0e9
Ross Light <ross@zombiezen.com>
2022-05-03 15:41:32 +0000

    install.sh: apply --force on Coder
    Also move away from git.io URLs

--- install.sh
+++ install.sh
@@ -26,9 +26,9 @@ if [ ! "$(command -v chezmoi)" ]; then
   bin_dir="$HOME/.local/bin"
   chezmoi="$bin_dir/chezmoi"
   if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$bin_dir"
+    sh -c "$(curl -fsSL https://chezmoi.io/get)" -- -b "$bin_dir"
   elif [ "$(command -v wget)" ]; then
-    sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$bin_dir"
+    sh -c "$(wget -qO- https://chezmoi.io/get)" -- -b "$bin_dir"
   else
     echo "To install chezmoi, you must have curl or wget installed." >&2
     exit 1
@@ -39,4 +39,9 @@ fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 script_dir="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
-exec "$chezmoi" init --apply "--source=$script_dir"
+"$chezmoi" init --source="$script_dir"
+if [[ -z "$CODER_RUNTIME" ]]; then
+  "$chezmoi" apply
+else
+  "$chezmoi" apply --force
+fi
