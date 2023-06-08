32c74af9645761da83a4d92d7dcef02978017b8e
Ross Light <ross@zombiezen.com>
2023-06-08 15:51:19 -0700

    nix: install Nix on work

--- nix/flake.nix
+++ nix/flake.nix
@@ -124,10 +124,19 @@
           {
             inherit (pkgs) wireshark;
           };
+        
+        workPackages = system:
+          let
+            pkgs = self.lib.nixpkgs system;
+          in
+          {
+            inherit (pkgs) nix;
+          };
 
         mypkgs = { system, discord ? false, gui ? false }:
           self.lib.basePackages system //
           nixpkgs.lib.optionalAttrs gui (self.lib.baseGUIPackages system) //
+          nixpkgs.lib.optionalAttrs discord (self.lib.workPackages system) //
           nixpkgs.lib.optionalAttrs (!discord) (self.lib.personalPackages system) //
           nixpkgs.lib.optionalAttrs (gui && !discord) (self.lib.personalGUIPackages system);
 
