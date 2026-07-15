99718e253b4c1d1be3d6cd8e9b7e55fb57b24212
Roxy Light <roxy@zombiezen.com>
2026-07-15 11:46:03 -0700

    .zshrc: fix bug in .zsh_history move script
    `test -f` resolves symlinks, so the script was not idempotent.
    `! test -h` does the correct thing.

--- run_before_move_zsh_history.sh
+++ run_before_move_zsh_history.sh
@@ -2,7 +2,7 @@
 # Move ~/.zsh_history to $XDG_STATE_HOME/zsh_history.
 # This is to migrate existing machines to my new preferred location.
 set -euo pipefail
-if [[ -f .zsh_history ]]; then
+if [[ ! -h .zsh_history ]]; then
   cat .zsh_history >> .local/state/zsh_history
   rm .zsh_history
 fi
